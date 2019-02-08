9f481a6816cfbbd7b5035bd17f953c43fe1279af
Bishop Shaw III <prototype124@yahoo.com>
2019-02-07 19:42:31 -0700

    update dropdown nav and more media queries

--- public/js/anotherjs.js
+++ public/js/anotherjs.js
@@ -1,9 +1,14 @@
  $(document).ready(function(){
-  $('.sidenav').sidenav();
+     $(".nav-trigger").on("click", () =>{
+         $(".sub-nav").toggle();
+     })
   });
   
   
-   
+//    let a = document.querySelector(".nav-trigger")
+//    a.addEventListener("click", () =>{
+//        alert("this button was clicked")
+//    })
 
   
 
