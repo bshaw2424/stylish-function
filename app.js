0a05eb89577e6db90c18c20fc9b01e6be7f4ada4
Bishop Shaw III <prototype124@yahoo.com>
2019-02-15 18:55:01 -0700

    media queries finish..start of adding javascript

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get("/table", (req, res) =>{
     res.render("pages/table")
 })
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log(`Server is running on port 3000`);
+app.listen(/*process.env.PORT || */ 8080, () => {
+    console.log(`Server is running on port 8080`);
 });
--- public/js/anotherjs.js
+++ public/js/anotherjs.js
@@ -1,15 +1,40 @@
  $(document).ready(function(){
+     //open and close mobile navigation...hamburger icon
      $(".nav-trigger").on("click", () =>{
          $(".sub-nav").toggle();
      })
   });
   
-  
-//    let a = document.querySelector(".nav-trigger")
-//    a.addEventListener("click", () =>{
-//        alert("this button was clicked")
-//    })
+//info section keyframes
+let page = document.querySelector("#about-section");
+window.addEventListener("scroll", function(e){
+    let wind = window.screenY;
+    if (wind === "560px") {
+        page.style.display = "none"
+    }
+})
 
+
+
+// sticky navigation bar
+let nav = document.querySelector("#navigation");
+
+window.addEventListener("scroll", stickyNavbar);
+
+function stickyNavbar() {
+    var sticky = nav.offsetTop;
+    console.log(nav.offsetTop)
+    if (window.pageYOffset !== sticky) {
+        nav.classList.add("sticky")
+        nav.style.background = "rgba(0,0,0, 0.5)"
+        nav.style.transition = "background 2.5s ease-in"
+      } else {
+        nav.style.background = "#ee6e73";
+        nav.classList.remove("sticky");
+        nav.style.transition = "background 1s ease-out"
+        
+      }
+};
   
 
       
