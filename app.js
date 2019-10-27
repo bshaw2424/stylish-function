18fe69e9e5d3f92adb8bb0fb47c0cb65cae39752
Bishop Shaw III <prototype124@yahoo.com>
2019-10-27 16:12:42 -0700

    restarted, refactored, and deleted files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dataBaseConnection = require('./mongodb_connect');
+const product = require('./models/productsModel');
+const featured = require('./models/featuredProductModel');
+const trending = require('./models/trendProductModel');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes/mainRoutes');
-const seatingRoutes = require('./routes/seatingRoutes');
-const tableRoutes = require('./routes/tableRoutes');
-const static = require('./routes/staticRoutes');
+
+// const static = require('./routes/staticRoutes');
 const errorRoutes = require('./routes/errorRoute');
 
 const errorRoute = new errorRoutes();
@@ -21,9 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
 // route middleware
-app.use('/', routes, static);
-app.use('/seating', seatingRoutes);
-app.use('/tables', tableRoutes);
+
 app.use(errorRoute.error404Route);
 
 // server connection
