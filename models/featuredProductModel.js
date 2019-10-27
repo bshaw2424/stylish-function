30ebad1957c7e0ed322f883ff13f46ef4696b55e
Bishop Shaw III <prototype124@yahoo.com>
2019-10-27 16:08:27 -0700

    refactored mongoose models

---
+++ models/featuredProductModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const featureSchema = require("../Schema/featuredProductSchema");
+const featureModel = mongoose.model("featured", featureSchema);
+
+const featuredProducts = new featureModel({
+	title: 'featured example title',
+	main_image: 'https://bit.ly/2phYaBh',
+	sub_images: null,
+	price: 4234
+});
+
+// featuredProducts.save( async(err, feature) =>{
+//   try {
+//     const data = await feature;
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
+
+module.exports = featureModel;
