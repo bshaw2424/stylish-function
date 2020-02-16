579a18427ced0e6d4066b2c78245b37eb5a6d767
Bishop Shaw III <prototype124@yahoo.com>
2020-02-16 16:52:56 -0700

    updated admin controller and routes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,85 @@
+const productsModel = require("../models/seatingTableProductModel");
+
 class AdminRoutes {
-	constructor() {}
+	adminLogIn(req, res) {
+		res.render("admin/login");
+	}
+	async adminMainIndexPage(req, res) {
+		try {
+			const allProducts = await productsModel.find().exec();
+			res.render("admin/admin-index", { allProducts });
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
+	async adminProductIndexPage(req, res) {
+		try {
+			const indexProducts = await productsModel.find().exec();
+			res.render("admin/products-index", { indexProducts });
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
+	adminProductForm(req, res) {
+		res.render("admin/products-form");
+	}
+	async adminProductPostForm(req, res) {
+		try {
+			await productsModel.create(req.body.product);
+			res.redirect("/admin/index");
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
+	async adminProductShowPage(req, res) {
+		try {
+			const product = await productsModel.findById(req.params.id).exec();
+			res.render("admin/productShowPage", { product });
+		} catch (error) {
+			if (error) {
+				res.redirect("/admin/product-index");
+			}
+		}
+	}
+	async adminProductEditForm(req, res) {
+		try {
+			const product = await productsModel.findById(req.params.id).exec();
+			res.render("admin/product_edit", { product });
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
+	async adminUpdateProduct(req, res) {
+		try {
+			const put_request = await productsModel.findByIdAndUpdate(
+				req.params.id,
+				req.body.product,
+			);
+			res.redirect(`/admin/products/${req.params.id}`);
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
+	async adminDeleteProduct(req, res) {
+		try {
+			await productsModel.findByIdAndRemove(req.params.id);
+			res.redirect("/admin/products");
+		} catch (error) {
+			if (error) {
+				res.render("pages/error404Page");
+			}
+		}
+	}
 }
 
 module.exports = AdminRoutes;
--- routes/admin/adminProductRoutes.js
+++ routes/admin/adminProductRoutes.js
@@ -1,103 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const products = require('../../models/seatingTableProductModel');
 
+const adminMethods = require("../../controllers/adminController");
+const Admin = new adminMethods();
 
-router.get("/", (req, res) => res.render("admin/login"));
-
-router.get("/index", async (req, res) =>{
-  try {
-    const allProducts = await products.find().exec();
-    res.render("admin/admin-index", { allProducts })
-  } catch (error) {
-    if(error){
-      res.render("pages/error404Page")
-    }
-  }
-    
-});
-
-// index product route
-router.get("/products", async (req, res) =>{
-  try {
-    const indexProducts = await products.find({}).exec();
-    res.render("admin/products-index", { indexProducts });
-  } catch (error) {
-    if(error){
-     res.render("pages/error404Page")
-    }
-  }
- 
-});
-
-// show new product form
-router.get("/products/new", (req, res) =>{
-  res.render("admin/products-form")
-});
-
-// create a new product
-router.post("/products", async (req, res) =>{
-  try {
-    await products.create(req.body.product);
-    res.redirect("/admin/index")
-  } catch (error) {
-    if(error){
-      console.log(error)
-      res.render("pages/error404Page")
-    }
-  }
-});
-
-// product show page
-router.get("/products/:id", async (req, res) =>{
-  try {
-    const product = await products.findById(req.params.id).exec();
-    res.render("admin/productShowPage", { product })
-  } catch (error) {
-    if(error){
-      res.redirect("/admin/product-index")
-    }
-  }
-});
-
-// edit product form
-router.get("/products/:id/edit", async (req, res) => {
-  try {
-    const product = await products
-					.findById(req.params.id)
-          .exec();
-    res.render("admin/product_edit", { product });
-  } catch (error) {
-    if(error){
-      res.render("pages/error404Page");
-    }
-  }
-});
-
-// update a product / redirect
-router.put("/products/:id", async (req, res) =>{
-  try {
-    const put_request = await products.findByIdAndUpdate(req.params.id, req.body.product);
-    console.log(put_request)
-    res.redirect(`/admin/products/${req.params.id}`)
-  } catch (error) {
-    if(error){
-      res.render("pages/error404Page")
-    }
-  }
-});
-
-// delete a product / redirect
-router.delete("/products/:id", async (req, res) =>{
-  try {
-     await products.findByIdAndRemove(req.params.id);
-    console.log(`${req.params.id} was removed successfull!`);
-    res.redirect('/admin/products');
-  } catch (error) {
-    if(error){
-      res.render("pages/error404Page")
-    }
-  }
-});
+router.get("/", Admin.adminLogIn);
+router.get("/index", Admin.adminMainIndexPage);
+router.get("/products", Admin.adminProductIndexPage);
+router.get("/products/new", Admin.adminProductForm);
+router.post("/products", Admin.adminProductPostForm);
+router.get("/products/:id", Admin.adminProductShowPage);
+router.get("/products/:id/edit", Admin.adminProductEditForm);
+router.put("/products/:id", Admin.adminUpdateProduct);
+router.delete("/products/:id", Admin.adminDeleteProduct);
 
 module.exports = router;
