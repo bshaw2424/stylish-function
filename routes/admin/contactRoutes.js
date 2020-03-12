219a1e3c3404044b4de3933b44932dc667379a52
Bishop Shaw III <prototype124@yahoo.com>
2020-03-11 19:50:38 -0700

    contact routes

---
+++ routes/admin/contactRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const contacts = require("../../controllers/contactController");
+
+router.get("/messages", contacts.index);
+router.get("/messages/new", contacts.create);
+router.post("/messages", contacts.post);
+router.get("/messages/:id", contacts.showPage);
+router.delete("/messages/:id", contacts.delete);
+
+module.exports = router;
