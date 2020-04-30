90dee426914d0371a134dcdc75428cce88064733
Bishop Shaw III <prototype124@yahoo.com>
2020-04-29 18:37:50 -0700

    error fixes

--- routes/featured/featuredRoutes.js
+++ routes/featured/featuredRoutes.js
@@ -3,12 +3,7 @@ const router = express.Router();
 
 const Featured = require("../../controllers/featuredController");
 
-router.get("/featured-article", Featured.index);
 router.get("/featured-article/new", Featured.create);
 router.post("/featured-article", Featured.post);
-router.get("/featured-article/:id", Featured.showPage);
-router.get("/featured-article/:id/edit", Featured.edit);
-router.put("/featured-article/:id", Featured.update);
-router.delete("/featured-article/:id", Featured.delete);
 
 module.exports = router;
