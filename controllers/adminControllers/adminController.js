f8b361bdebdaf03d8ce871613b8bd9f29b6837f5
Bishop Shaw III <prototype124@yahoo.com>
2020-05-07 18:22:09 -0700

    organize all admin controllers into a single directory

---
+++ controllers/adminControllers/adminController.js
@@ -0,0 +1,22 @@
+const { SeatingModel, TablesModel } = require("../../models/products");
+
+exports.logIn = (req, res) => {
+	res.render("admin/login");
+};
+
+exports.logOut = (req, res) => {
+	res.render("admin/logout");
+};
+
+exports.index = async (req, res) => {
+	try {
+		const seatingProducts = await SeatingModel.find().exec();
+		const tableproducts = await TablesModel.find().exec();
+		res.render("admin/adminIndex", { seatingProducts, tableproducts });
+	} catch (error) {
+		if (error) {
+			console.log(error);
+			res.render("pages/error404Page");
+		}
+	}
+};
