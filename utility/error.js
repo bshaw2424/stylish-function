c0413e0cf038c93ec2fe775a768714e8bb78fe4a
Bishop Shaw III <prototype124@yahoo.com>
2020-11-15 19:18:18 -0700

    moved error functions to a utility folder

---
+++ utility/error.js
@@ -0,0 +1,9 @@
+module.exports.errorMessage = (req, res) => {
+	res.status(404).render("pages/error404Page");
+};
+
+module.exports.asyncError = fn => {
+	return (req, res, next) => {
+		fn(req, res, next).catch((error) => next(error));
+	};
+};
