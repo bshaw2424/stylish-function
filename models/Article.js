b25509a220e723711367507902ecb794dab58fb3
Bishop Shaw III <prototype124@yahoo.com>
2021-07-30 18:04:22 -0700

    Updated models/Article.js

---
+++ models/Article.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const slugify = require("slugify");
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  sub_description: {
+    type: String,
+    trim: true,
+    require: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  created_on: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+articleSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+module.exports = model("Article", articleSchema);
