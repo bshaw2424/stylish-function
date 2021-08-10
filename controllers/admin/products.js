9beed35d7269cb285a613fd90a273dcbf4496041
Bishop Shaw III <prototype124@yahoo.com>
2021-08-09 20:40:46 -0700

    Added products routes to edit page

--- controllers/admin/products.js
+++ controllers/admin/products.js
@@ -1,44 +1,37 @@
 const Article = require("../../models/Article");
-const Product = require("../../models/Product");
 
-// module.exports.create = async (req, res) => {
-//   const { id } = req.params;
-//   res.render("admin/products/create", { id });
-// };
+module.exports.showPage = async (req, res, next) => {
+  await res.render("admin/products/showPage", { articles });
+};
 
-// module.exports.post = async (req, res, next) => {
-//   const { Products } = req.body;
-//   const { id } = req.params;
-//   const Doc = Article.findById(id);
-//   console.log(Doc);
-//   res.redirect(`/admin/articles/${id}`);
-// };
-
-// module.exports.showPage = async (req, res, next) => {
-//   await res.render("admin/products/showPage", { articles });
-// };
+module.exports.new = async (req, res, next) => {
+  const { id } = req.params;
+  const articles_product = await Article.findById(id);
+  console.log(articles_product);
+  res.render("admin/products/create", { id });
+};
 
 module.exports.edit = async (req, res, next) => {
-  const { id } = req.params;
-  res.render("admin/products/edit", { id });
+  const { id, product } = req.params;
+  // finds the parent document by id
+  const articles_product = await Article.findById(id);
+  // finds the subdocument array id
+  const abby = articles_product.products.id(product);
+  res.render("admin/products/edit", { articles_product, abby });
 };
 
 module.exports.update = async (req, res, next) => {
   const { id } = req.params;
   const { Products } = req.body;
-  const article_products = await Article.findByIdAndUpdate(
-    id,
-    { $push: { products: { Products } } },
-    {
-      new: true,
-    },
-  );
+  const article_products = await Article.findByIdAndUpdate(id);
+  article_products.products.push(Products);
   await article_products.save();
+  console.log(article_products);
   res.redirect(`/admin/articles/${id}`);
 };
 
 module.exports.delete = async (req, res, next) => {
   const { id } = req.params;
   await Article.findByIdAndDelete(id);
-  res.redirect(`/admin/articles/${id}/products`);
+  res.redirect(`/admin/articles/${id}`);
 };
