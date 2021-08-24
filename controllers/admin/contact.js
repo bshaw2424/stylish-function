5d1ad3ed2c9f2582b252fb9ac1bd741c57a3ee7b
Bishop Shaw III <prototype124@yahoo.com>
2021-08-24 15:57:37 -0700

    updated post route in file

--- controllers/admin/contact.js
+++ controllers/admin/contact.js
@@ -2,21 +2,22 @@ const ContactModel = require("../../models/Contacts");
 
 module.exports.index = async (req, res, next) => {
   const messages = await ContactModel.find();
-  res.render("admin/contact/contact", { messages });
+  res.render("admin/contacts/contact", { messages });
 };
 
-module.exports.create = (req, res) => res.render("admin/contact/contactUs");
+module.exports.create = (req, res) => res.render("admin/contacts/contactUs");
 
 module.exports.post = async (req, res, next) => {
   const { Message } = req.body;
-  await ContactModel.create(Message);
+  const newMessage = new ContactModel(Message);
+  await newMessage.save();
   res.redirect("/contact-us");
 };
 
 module.exports.showPage = async (req, res, next) => {
   const { id } = req.params;
   const message = await ContactModel.findById(id);
-  res.render("admin/contact/contactShowPage", { message });
+  res.render("admin/contacts/contactShowPage", { message });
 };
 
 module.exports.delete = async (req, res, next) => {
