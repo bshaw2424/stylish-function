d3fa31570761dacb3f86b53d54dd3f2743319493
Bishop Shaw III <prototype124@yahoo.com>
2021-09-06 20:15:36 -0700

    updated script file to add defer tag

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,29 +1,8 @@
-const filterTitles = document.querySelectorAll(".category");
-const sortFilterList = document.querySelectorAll(".sort");
-const sortNav = document.querySelectorAll(".sort-nav");
-const category = document.querySelectorAll(".category-nav");
+const messageCheckBox = document.querySelectorAll(".message-checkbox");
+const trashcan = document.querySelectorAll("message-center_delete");
 
-// event listeners
-filterTitles.forEach((item) => item.addEventListener("click", showCategory));
-sortFilterList.forEach((item) => item.addEventListener("click", showSortList));
-
-// event functions
-function showCategory() {
-	toggleList(category);
-}
-
-function showSortList() {
-	toggleList(sortNav);
-}
-
-function toggleList(ele, e) {
-	ele.forEach((e) => {
-		if (e.classList.toggle("hide")) {
-			console.log(e.classList);
-		} else {
-			console.log(false);
-		}
-	});
-}
-
-//
+messageCheckBox.forEach(item => {
+  item.addEventListener("click", e => {
+    console.log(e);
+  });
+});
