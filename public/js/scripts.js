e8abf431563a39fdc2d304b0723648e4ebd551e2
Bishop Shaw III <prototype124@yahoo.com>
2021-09-14 20:49:40 -0700

    added functions to scripts.js file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,7 @@ const messageCheckBox = document.querySelectorAll(".collection-item");
 const messagebox = document.querySelectorAll(".message-checkbox");
 const delete_all_checkbox = document.getElementById("delete-all");
 let message_counter = document.getElementById("message-checkbox-counter");
+const delete_message = document.getElementById("delete-all-icon");
 
 messageCheckBox.forEach(function (messageItem) {
   messageItem.addEventListener("mouseenter", e => {
@@ -19,10 +20,14 @@ messageCheckBox.forEach(function (messageItem) {
 delete_all_checkbox.addEventListener("change", function (e) {
   if (e.target.checked === false) {
     checkAllCheckBoxes(false);
+    messageCounter();
   } else {
     checkAllCheckBoxes();
+    messageCounter();
   }
 });
+
+delete_message.addEventListener("click", () => alert("this was clicked"));
 
 messagebox.forEach(checkbox => {
   checkbox.addEventListener("change", e => {
@@ -34,7 +39,7 @@ function checkAllCheckBoxes(checked = true) {
   messagebox.forEach(checkedbox => (checkedbox.checked = checked));
 }
 
-function messageCounter(id) {
+function messageCounter() {
   let counter = [];
   for (let i = 0; i < messagebox.length; i++) {
     if (messagebox[i].checked === true) {
@@ -46,4 +51,12 @@ function messageCounter(id) {
   } else {
     message_counter.innerText = " - ";
   }
+  console.log(counter);
 }
+
+function deleteAllCheckedMessages(id) {
+  const item = document.querySelectorAll(".collection-item-info");
+  const a = messagebox.closest(item);
+  console.log(a);
+}
+deleteAllCheckedMessages();
