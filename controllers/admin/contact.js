583816709f47c5cbb85ba9fdda5478a760efc239
Bishop Shaw III <prototype124@yahoo.com>
2021-09-19 21:56:57 -0700

    added additional crud routes

--- controllers/admin/contact.js
+++ controllers/admin/contact.js
@@ -1,26 +1,41 @@
+const { findById } = require("../../models/Contacts");
 const ContactModel = require("../../models/Contacts");
 
-module.exports.index = async (req, res, next) => {
+module.exports.index = async (req, res) => {
   const messages = await ContactModel.find();
   res.render("admin/contacts/contact", { messages });
 };
 
 module.exports.create = (req, res) => res.render("admin/contacts/contactUs");
 
-module.exports.post = async (req, res, next) => {
+module.exports.post = async (req, res) => {
   const { Message } = req.body;
   const newMessage = new ContactModel(Message);
   await newMessage.save();
   res.redirect("/contact-us");
 };
 
-module.exports.showPage = async (req, res, next) => {
+module.exports.contact_post = async (req, res) => {
+  const { message } = req.body;
+  const runt = await ContactModel.find({});
+  const turn = runt.map(a => a._id);
+
+  let gran = [];
+  for (let i = 0; i < message.length; i++) {
+    gran.push({ _id: turn[i], message_value: Boolean(message[i]) });
+  }
+  console.log(gran);
+
+  res.redirect("/admin/messages");
+};
+
+module.exports.showPage = async (req, res) => {
   const { id } = req.params;
   const message = await ContactModel.findById(id);
   res.render("admin/contacts/contactShowPage", { message });
 };
 
-module.exports.delete = async (req, res, next) => {
+module.exports.delete = async (req, res) => {
   const { id } = req.params;
   await ContactModel.findByIdAndDelete(id);
   res.redirect("/admin/messages");
