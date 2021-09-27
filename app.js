c44fb9a2aec3f7a544d86a630b6c0522970b487e
Bishop Shaw III <prototype124@yahoo.com>
2021-09-26 22:07:44 -0700

    updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require("method-override");
 const database = require("./mongoDatabase");
 const path = require("path");
 const asyncError = require("./utility/error");
+const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 
 // imported routes
@@ -12,7 +13,6 @@ const adminIndexRoutes = require("./routes/admin");
 const articleRoutes = require("./routes/admin/articles");
 const productRoutes = require("./routes/admin/products");
 const contactRoutes = require("./routes/admin/contact");
-const usersRoutes = require("./routes/admin/users");
 const staticRoutes = require("./routes/index/static");
 const mainArticleRoutes = require("./routes/index/articles");
 const errorRoutes = require("./utility/error");
@@ -22,6 +22,13 @@ const bcrypt = require("./bcrypt");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "lookingtogetthisstarted",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -29,7 +36,6 @@ app.use("/admin", adminIndexRoutes);
 app.use("/admin/articles", articleRoutes);
 app.use("/admin/articles/:id/products", productRoutes);
 app.use("/admin/messages", contactRoutes);
-app.use("/admin/users", usersRoutes);
 app.use("/articles", mainArticleRoutes);
 app.use("/", staticRoutes);
 app.use(errorRoutes.errorMessage);
