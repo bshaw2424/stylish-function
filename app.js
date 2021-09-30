1a46a2306f9ea8719fed92a13c217846a9a3d58c
Bishop Shaw III <prototype124@yahoo.com>
2021-09-29 19:01:00 -0700

    updated app.js

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 passport.use(new LocalStrategy(User.authenticate()));
 
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
