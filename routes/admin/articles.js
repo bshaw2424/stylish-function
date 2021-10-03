3f2fca7f1eea34c044a948775faed67856b21de3
Bishop Shaw III <prototype124@yahoo.com>
2021-10-02 19:47:23 -0700

    update middleware in file

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { asyncError } = require("../../utility/error");
+const session = require("express-session");
+const { checkAuthentication } = require("../../middleware");
+const multer = require("multer");
+const { storage } = require("../../cloudinary");
+const upload = multer({ storage });
 
-const Articles = require("../../controllers/admin/articles");
+const Article = require("../../controllers/admin/articles");
 
-router.get("/", asyncError(Articles.index));
-router.get("/new", Articles.create);
-router.post("/", asyncError(Articles.post));
-router.get("/:id", asyncError(Articles.showPage));
-router.get("/:id/edit", asyncError(Articles.edit));
-router.put("/:id", asyncError(Articles.update));
-router.delete("/:id", asyncError(Articles.delete));
+router
+  .route("/")
+  .get(checkAuthentication, asyncError(Article.index))
+  .post(
+    upload.single("Article[image]"),
+    checkAuthentication,
+    asyncError(Article.post),
+  );
+router.get("/new", checkAuthentication, Article.create);
+router
+  .route("/:id")
+  .get(checkAuthentication, asyncError(Article.showPage))
+  .put(checkAuthentication, asyncError(Article.update))
+  .delete(checkAuthentication, asyncError(Article.delete));
+router.get(
+  "/:id/edit",
+  upload.single("Article[image]"),
+  checkAuthentication,
+  asyncError(Article.edit),
+);
 
 module.exports = router;
