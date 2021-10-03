48eacf409ec779f373d063cc0df4edd9a20d196f
Bishop Shaw III <prototype124@yahoo.com>
2021-10-02 19:45:06 -0700

    update envirnoment variables

--- mongoDatabase.js
+++ mongoDatabase.js
@@ -2,19 +2,15 @@ const mongoose = require("mongoose");
 const ENV = require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.set("useFindAndModify", false);
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const MONGODB_URL = `mongodb+srv://bshaw-stylefunction:${password}@cluster0.3kkqi.mongodb.net/${user}`;
+const MONGODB_URL = `mongodb+srv://bshaw-stylefunction:${process.env.DB_PASSWORD}@cluster0.3kkqi.mongodb.net/${process.env.DB_USER}`;
 
 const databaseConnection = mongoose
-	.connect(MONGODB_URL, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("Database connected"))
-	.catch((err) => console.log(`Error: ${err.message}`));
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(`Error: ${err.message}`));
 
 module.exports = databaseConnection;
