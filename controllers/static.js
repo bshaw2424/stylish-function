1193acf269f44f3483ab6a6e8dc7c273bcde57d9
Bishop Shaw III <prototype124@yahoo.com>
2021-10-17 18:31:00 -0700

    updated static controller

--- controllers/static.js
+++ controllers/static.js
@@ -1,15 +1,20 @@
-exports.index = (req, res) => res.render("pages/index");
+const ArticleModel = require("../models/Article");
+
+module.exports.index = async (req, res) => {
+  const articles = await ArticleModel.find({});
+  res.render("pages/index", { articles });
+};
 
 exports.aboutUs = (req, res) => res.render("static/aboutUs");
 
 exports.affiliateDisclaimer = (req, res) => res.render("static/about-us");
 
 exports.affiliateDisclaimer = (req, res) =>
-	res.render("static/affiliateDisclaimer");
+  res.render("static/affiliateDisclaimer");
 
 exports.contactUs = (req, res) => res.render("static/contactUs");
 
 exports.privacyDisclosure = (req, res) =>
-	res.render("static/privacy-disclosure");
+  res.render("static/privacy-disclosure");
 
 exports.termsOfService = (req, res) => res.render("static/TOS");
