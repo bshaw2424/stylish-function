ec290016dcb69fe9fb0873ea7621a87add4344cc
Bishop Shaw III <prototype124@yahoo.com>
2022-01-21 18:37:45 -0700

    added mobile and tablet slide out functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,13 +6,21 @@ const collection_items = document.querySelectorAll(".collection-item");
 const cardTitles = document.querySelectorAll(".card-content .card-title");
 const card = document.querySelectorAll(".article-container .col");
 
-const filterInput = document.getElementById("filter-search");
+const filterInput = document.querySelector(".filter-search #filter-search");
+const hamburgerNav = document.getElementById("hamburger-nav-button");
+const navLinks = document.querySelector(".index-navigation-links");
 
 (() => {
   collectionsButtonShowOnHover();
   collectionsButtonHideOnHover();
+  productFilter();
+  window.addEventListener("load", () => {
+    console.log("button was clicked");
+  });
 })();
-
+hamburgerNav.addEventListener("click", () => {
+  navLinks.classList.toggle("hidden");
+});
 function collectionsButtonShowOnHover() {
   for (let i = 0; i < collection_items.length; i++) {
     const collection = collection_items[i];
@@ -33,34 +41,29 @@ function collectionsButtonHideOnHover() {
   }
 }
 
-function nextMessage() {
-  console.log("article was not found");
-}
+function productFilter() {
+  filterInput.addEventListener("input", e => {
+    const titles = Array.from(cardTitles).slice();
+    const cards = Array.from(card).slice();
 
-filterInput.addEventListener("input", e => {
-  const titles = Array.from(cardTitles).slice();
-  const cards = Array.from(card).slice();
+    const targetValue = e.target.value.toLowerCase();
 
-  let targetValue = e.target.value.toLowerCase();
+    const results = [];
+    const cardDisplay = [];
 
-  let results = [];
-  let cardDisplay = [];
-
-  for (let i = 0; i < titles.length; i++) {
-    results.push(titles[i]);
-    cardDisplay.push(cards[i]);
-  }
-
-  const findCards = results.filter((item, index) => {
-    console.log(results.length);
-
-    if (item.innerText.toLowerCase().includes(targetValue)) {
-      cardDisplay[index].style.display = "";
-    } else {
-      cardDisplay[index].style.display = "none";
-      console.log(cardDisplay.length);
+    for (let i = 0; i < titles.length; i++) {
+      results.push(titles[i]);
+      cardDisplay.push(cards[i]);
     }
-  });
 
-  return findCards;
-});
+    const findCards = results.filter((item, index) => {
+      if (item.innerText.toLowerCase().includes(targetValue)) {
+        cardDisplay[index].style.display = "";
+      } else {
+        cardDisplay[index].style.display = "none";
+      }
+    });
+
+    return findCards;
+  });
+}
