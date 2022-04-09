2c2ccd87132d2b914b14305b5dbf4873537e070a
Bishop Shaw III <prototype124@yahoo.com>
2022-04-08 19:02:50 -0700

    models directory folder

---
+++ models/Contacts.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const {
+  Schema,
+  model
+} = mongoose;
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  message_value: {
+    type: Boolean,
+    default: false
+  },
+  created_on: {
+    type: Date,
+    default: new Date()
+  }
+});
+module.exports = model("contact", contactSchema);
