72109a11ae05ebfec99bf36db7fd5deb13b841f4
Bishop Shaw III <prototype124@yahoo.com>
2022-04-08 19:01:58 -0700

    middlewae.js file

---
+++ middleware.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports.checkAuthentication = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    next();
+  } else {
+    res.redirect("/admin/login");
+  }
+};
