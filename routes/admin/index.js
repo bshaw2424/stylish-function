b0412d66758c6c4eefc3d1d3c52ab617b1004636
Bishop Shaw III <prototype124@yahoo.com>
2022-04-23 18:36:38 -0700

    admin index route

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,20 +6,21 @@ const router = express.Router();
 
 const passport = require("passport");
 
-const {
-  AsyncError
-} = require("../../utility/error");
+const { AsyncError } = require("../../utility/error");
 
 const Admin = require("../../controllers/admin/admin");
 
-const {
-  response
-} = require("express");
+const { response } = require("express");
 
 router.get("/dashboard", AsyncError(Admin.index));
 router.get("/login", AsyncError(Admin.login));
-router.post("/login", passport.authenticate("local", {
-  failureRedirect: "/admin/login"
-}), AsyncError(Admin.post));
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/admin/dashboard",
+    failureRedirect: "/admin/login",
+  }),
+  AsyncError(Admin.post),
+);
 router.get("/logout", AsyncError(Admin.logout));
 module.exports = router;
