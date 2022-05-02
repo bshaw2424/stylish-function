fcdf109ce495ace61c7dbeb3f36d5f8b078ea1ca
Bishop Shaw III <prototype124@yahoo.com>
2022-05-01 18:53:58 -0700

    static controller

--- controllers/admin/static.js
+++ controllers/admin/static.js
@@ -1,13 +1,13 @@
 "use strict";
 const ENV = require("dotenv").config();
+const site = process.env.RECAPTCHA_KEY;
 
 module.exports.aboutUs = (req, res) => res.render("pages/static/about-us");
 
 module.exports.affiliateDisclaimer = (req, res) =>
   res.render("pages/static/affiliateDisclaimer");
 
-module.exports.contactUs = (req, res) =>
-  res.render("static/contactUs", { site_key: process.env.RECAPTCHA_KEY });
+module.exports.contactUs = (req, res) => res.render("static/contactUs");
 
 module.exports.contactUsSuccess = (req, res) =>
   res.render("pages/static/success");
