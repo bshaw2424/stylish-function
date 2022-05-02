7ec1bf1d3a846369bfb588b59020283fc1750678
Bishop Shaw III <prototype124@yahoo.com>
2022-05-01 18:07:43 -0700

    contact us section - controller and views

--- controllers/admin/static.js
+++ controllers/admin/static.js
@@ -1,13 +1,18 @@
 "use strict";
+const ENV = require("dotenv").config();
 
 module.exports.aboutUs = (req, res) => res.render("pages/static/about-us");
 
-module.exports.affiliateDisclaimer = (req, res) => res.render("pages/static/affiliateDisclaimer");
+module.exports.affiliateDisclaimer = (req, res) =>
+  res.render("pages/static/affiliateDisclaimer");
 
-module.exports.contactUs = (req, res) => res.render("pages/static/contactUs");
+module.exports.contactUs = (req, res) =>
+  res.render("static/contactUs", { site_key: process.env.RECAPTCHA_KEY });
 
-module.exports.contactUsSuccess = (req, res) => res.render("pages/static/success");
+module.exports.contactUsSuccess = (req, res) =>
+  res.render("pages/static/success");
 
-module.exports.privacyDisclosure = (req, res) => res.render("pages/static/privacy-disclosure");
+module.exports.privacyDisclosure = (req, res) =>
+  res.render("pages/static/privacy-disclosure");
 
 module.exports.termsOfService = (req, res) => res.render("pages/static/TOS");
