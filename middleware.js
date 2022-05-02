4d1a9d4b93f757ccd400f6b5f92123d95523d9bf
Bishop Shaw III <prototype124@yahoo.com>
2022-05-01 20:10:54 -0700

    middleware update

--- middleware.js
+++ middleware.js
@@ -4,7 +4,5 @@ module.exports.checkAuthentication = (req, res, next) => {
   if (req.isAuthenticated()) {
     // req.session.returnTo = req.originalUrl;
     next();
-  } else {
-    res.redirect("/admin/login");
   }
 };
