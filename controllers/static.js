d99f7253c0e5d6eb1c00aaa4594ede531273e595
Bishop Shaw III <prototype124@yahoo.com>
2022-05-15 19:19:44 -0700

    static controller file

--- controllers/static.js
+++ controllers/static.js
@@ -3,26 +3,30 @@
 const ArticleModel = require("../models/Article");
 
 module.exports.index = async (req, res) => {
-  const articles = await ArticleModel.find().sort({
-    created_on: "DESC"
-  }) //sort by newest article
-  .limit(4); //show only first four articles
+  const articles = await ArticleModel.find()
+    .sort({
+      created_on: "DESC",
+    })
+    .limit(4);
 
   res.render("pages/index", {
-    articles
+    articles,
   });
 };
 
 module.exports.aboutUs = (req, res) => res.render("static/aboutUs");
 
-module.exports.affiliateDisclaimer = (req, res) => res.render("static/about-us");
+module.exports.affiliateDisclaimer = (req, res) =>
+  res.render("static/about-us");
 
-module.exports.affiliateDisclaimer = (req, res) => res.render("static/affiliateDisclaimer");
+module.exports.affiliateDisclaimer = (req, res) =>
+  res.render("static/affiliateDisclaimer");
 
 module.exports.contactUs = (req, res) => res.render("static/contactUs");
 
 module.exports.contactUsSuccess = (req, res) => res.render("static/success");
 
-module.exports.privacyDisclosure = (req, res) => res.render("static/privacy-disclosure");
+module.exports.privacyDisclosure = (req, res) =>
+  res.render("static/privacy-disclosure");
 
 module.exports.termsOfService = (req, res) => res.render("static/TOS");
