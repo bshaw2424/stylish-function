5c08ed5eab50e17430ad334a4db25464a00bb918
Bishop Shaw III <prototype124@yahoo.com>
2022-06-09 00:16:32 -0700

    index static routes

--- routes/index/static.js
+++ routes/index/static.js
@@ -7,8 +7,8 @@ const Static = require("../../controllers/static");
 router.get("/", Static.index);
 router.get("/about-us", Static.aboutUs);
 router.get("/affiliate-disclaimer", Static.affiliateDisclaimer);
-router.get("/contact", Static.contactUs);
-router.get("/contact/success", Static.contactUsSuccess);
+router.get("/contact", Static.contact);
+router.get("/contact/success", Static.contactSuccess);
 router.get("/privacy-disclosure", Static.privacyDisclosure);
 router.get("/TOS", Static.termsOfService);
 
