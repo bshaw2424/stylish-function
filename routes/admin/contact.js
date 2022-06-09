7ee10eb9445234d6273abf139ac34cb38ad6209b
Bishop Shaw III <prototype124@yahoo.com>
2022-06-08 23:48:50 -0700

    admin contact routes

--- routes/admin/contact.js
+++ routes/admin/contact.js
@@ -13,10 +13,6 @@ const Contact = require("../../controllers/admin/contact");
 adminRouter.get("/", checkAuthentication, AsyncError(Contact.index));
 adminRouter.get("/new", Contact.create);
 adminRouter.post("/", AsyncError(Contact.post));
-adminRouter.get("/sortDesc/:sort", AsyncError(Contact.descSort));
-adminRouter.get("/sortAsc/:sort", AsyncError(Contact.ascSort));
-adminRouter.get("/:id", AsyncError(Contact.showPage));
-adminRouter.delete("/:id", AsyncError(Contact.delete));
 
 app.use(subdomain("admin", adminRouter));
 module.exports = adminRouter;
