fd2f7fe5b0fb59dbea6f64ba49c6504960ad60ae
Bishop Shaw III <prototype124@yahoo.com>
2022-06-09 00:16:02 -0700

    index static controller

--- controllers/static.js
+++ controllers/static.js
@@ -17,14 +17,11 @@ module.exports.index = async (req, res) => {
 module.exports.aboutUs = (req, res) => res.render("static/aboutUs");
 
 module.exports.affiliateDisclaimer = (req, res) =>
-  res.render("static/about-us");
-
-module.exports.affiliateDisclaimer = (req, res) =>
   res.render("static/affiliateDisclaimer");
 
-module.exports.contactUs = (req, res) => res.render("static/contactUs");
+module.exports.contact = (req, res) => res.render("static/contactUs");
 
-module.exports.contactUsSuccess = (req, res) => res.render("static/success");
+module.exports.contactSuccess = (req, res) => res.render("static/success");
 
 module.exports.privacyDisclosure = (req, res) =>
   res.render("static/privacy-disclosure");
