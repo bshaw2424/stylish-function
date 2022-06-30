d2271b53212959e038e559d463b7874b1bda92fb
Bishop Shaw III <prototype124@yahoo.com>
2022-06-30 13:24:48 -0700

    index admin contact controller

--- controllers/admin/contact.js
+++ controllers/admin/contact.js
@@ -15,11 +15,10 @@ module.exports.post = async (req, res) => {
 
   const response = await fetch(verifyCaptchaResponseURL);
   const data = await response.json();
-  if (response.status === 200 && data.success === true) {
-    const newMessage = new ContactModel(Message);
-    await newMessage.save();
-    res.redirect("/contact/success");
-  }
+
+  const newMessage = new ContactModel(Message);
+  await newMessage.save();
+  res.redirect("/contact/success");
 };
 
 module.exports.ascSort = async (req, res) => {
