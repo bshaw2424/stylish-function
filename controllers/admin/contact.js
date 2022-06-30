d7ca9445ad0aa9f8c21716e47b76b1a64e8e313b
Bishop Shaw III <prototype124@yahoo.com>
2022-06-30 13:05:27 -0700

    index admin contact controller

--- controllers/admin/contact.js
+++ controllers/admin/contact.js
@@ -11,15 +11,15 @@ module.exports.post = async (req, res) => {
 
   // reCaptcha response token
   const reCaptchaBodyResponse = req.body["g-recaptcha-response"];
-
+  let newMessage;
   const verifyCaptchaResponseURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${reCaptchaBodyResponse}&remoteip=${req.connection.remoteAddress}`;
 
   const response = await fetch(verifyCaptchaResponseURL);
   const data = await response.json();
   if (response.status === 200 && data.success === true) {
-    const newMessage = new ContactModel(Message);
-    await newMessage.save();
+    newMessage = new ContactModel(Message);
   }
+  await newMessage.save();
   res.redirect("/contact/success");
 };
 
