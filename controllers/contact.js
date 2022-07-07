7afdb8aa8aaa6b4e8e08616964e3fc03ec7eb4a5
Bishop Shaw III <prototype124@yahoo.com>
2022-07-07 15:32:42 -0700

    contact controller

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const fetch = require("node-fetch");
-const ContactModel = require("../../models/Contacts");
+const ContactModel = require("../models/Contacts");
 require("dotenv").config();
 
-module.exports.create = (req, res) => res.render("admin/contacts/contactUs");
+module.exports.create = (req, res) => res.render("static/contactUs");
 
 module.exports.post = async (req, res) => {
   const { Message } = req.body;
@@ -19,30 +19,30 @@ module.exports.post = async (req, res) => {
   res.redirect("/contact/success");
 };
 
-module.exports.ascSort = async (req, res) => {
-  const ascendingSort = await ContactModel.find({}).sort({ created_on: 1 });
-  res.render("admin/contacts/contactAscendingSort", {
-    ascendingSort,
-    sortMessage: "All Clear...No Messages",
-  });
-};
+// module.exports.ascSort = async (req, res) => {
+//   const ascendingSort = await ContactModel.find({}).sort({ created_on: 1 });
+//   res.render("admin/contacts/contactAscendingSort", {
+//     ascendingSort,
+//     sortMessage: "All Clear...No Messages",
+//   });
+// };
 
-module.exports.descSort = async (req, res) => {
-  const descendingSort = await ContactModel.find({}).sort({ created_on: -1 });
-  res.render("admin/contacts/contactDescendingSort", {
-    descendingSort,
-    sortMessage: "All Clear...No Messages",
-  });
-};
+// module.exports.descSort = async (req, res) => {
+//   const descendingSort = await ContactModel.find({}).sort({ created_on: -1 });
+//   res.render("admin/contacts/contactDescendingSort", {
+//     descendingSort,
+//     sortMessage: "All Clear...No Messages",
+//   });
+// };
 
-module.exports.showPage = async (req, res) => {
-  const { id } = req.params;
-  const message = await ContactModel.findById(id);
-  res.render("admin/contacts/contactShowPage", { message });
-};
+// module.exports.showPage = async (req, res) => {
+//   const { id } = req.params;
+//   const message = await ContactModel.findById(id);
+//   res.render("admin/contacts/contactShowPage", { message });
+// };
 
-module.exports.delete = async (req, res) => {
-  const { id } = req.params;
-  const deleteMessage = await ContactModel.findByIdAndDelete(id);
-  res.redirect("/messages");
-};
+// module.exports.delete = async (req, res) => {
+//   const { id } = req.params;
+//   const deleteMessage = await ContactModel.findByIdAndDelete(id);
+//   res.redirect("/messages");
+// };
