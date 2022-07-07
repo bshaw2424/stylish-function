398bf23f79636dead34fb3693b4083090a17334f
Bishop Shaw III <prototype124@yahoo.com>
2022-07-07 16:05:57 -0700

    contact controller

--- controllers/contact.js
+++ controllers/contact.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const ContactModel = require("../models/Contacts");
 require("dotenv").config();
 
-module.exports.create = (req, res) => res.render("static/contactUs");
+module.exports.create = (req, res) => res.render("contactUs");
 
 module.exports.post = async (req, res) => {
   const { Message } = req.body;
@@ -18,31 +18,3 @@ module.exports.post = async (req, res) => {
   await newMessage.save();
   res.redirect("/contact/success");
 };
-
-// module.exports.ascSort = async (req, res) => {
-//   const ascendingSort = await ContactModel.find({}).sort({ created_on: 1 });
-//   res.render("admin/contacts/contactAscendingSort", {
-//     ascendingSort,
-//     sortMessage: "All Clear...No Messages",
-//   });
-// };
-
-// module.exports.descSort = async (req, res) => {
-//   const descendingSort = await ContactModel.find({}).sort({ created_on: -1 });
-//   res.render("admin/contacts/contactDescendingSort", {
-//     descendingSort,
-//     sortMessage: "All Clear...No Messages",
-//   });
-// };
-
-// module.exports.showPage = async (req, res) => {
-//   const { id } = req.params;
-//   const message = await ContactModel.findById(id);
-//   res.render("admin/contacts/contactShowPage", { message });
-// };
-
-// module.exports.delete = async (req, res) => {
-//   const { id } = req.params;
-//   const deleteMessage = await ContactModel.findByIdAndDelete(id);
-//   res.redirect("/messages");
-// };
