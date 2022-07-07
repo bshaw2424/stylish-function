8898b4362e29942796e97fc21725bf54bfa3c107
Bishop Shaw III <prototype124@yahoo.com>
2022-07-07 16:06:21 -0700

    middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 "use strict";
 
-module.exports.checkAuthentication = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    // req.session.returnTo = req.originalUrl;
-    next();
-  }
-};
+// module.exports.checkAuthentication = (req, res, next) => {
+//   if (!req.isAuthenticated()) {
+//     // req.session.returnTo = req.originalUrl;
+//     next();
+//   }
+// };
