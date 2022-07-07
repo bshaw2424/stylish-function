7252b5caa5ab2407551a300876932329baeee754
Bishop Shaw III <prototype124@yahoo.com>
2022-07-07 16:08:10 -0700

    remove unused middleware file

--- middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// module.exports.checkAuthentication = (req, res, next) => {
-//   if (!req.isAuthenticated()) {
-//     // req.session.returnTo = req.originalUrl;
-//     next();
-//   }
-// };
