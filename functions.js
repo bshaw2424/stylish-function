789a884e33c8f3198e829f1121bd44c2b4548752
Bishop Shaw III <prototype124@yahoo.com>
2022-07-07 15:39:27 -0700

    removed unused functions

--- functions.js
+++ functions.js
@@ -4,12 +4,5 @@ module.exports.convertPriceWithComma = price => {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-module.exports.deleteAjaxButton = async (url, method) => {
-  await fetch(url, { method });
-};
 
-module.exports.hide = () => {
-  console.log("this is working again!!!");
-};
 
-// module.exports.fetchAjax = async (url, method)
