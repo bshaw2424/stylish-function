d442338259c790a5353a4ae5f3f9fa0959103232
Bishop Shaw III <prototype124@yahoo.com>
2022-09-12 18:34:47 -0700

    updated controllers contact.js

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const { json } = require("body-parser");
 const fetch = require("node-fetch");
 const ContactModel = require("../models/Contacts");
+const { fetchContactData } = require("../functions");
 
 module.exports.create = (req, res) => res.render("contactUs");
 
@@ -14,12 +14,13 @@ module.exports.post = async (req, res) => {
   const verifyCaptchaResponseURL = `https://www.google.com/recaptcha/api/siteverify?secret=6LfDB7UfAAAAAAs1e_yxA1gprsTEuZn--7ihanyF&response=${reCaptchaBodyResponse}&remoteip=${req.connection.remoteAddress}`;
 
   const response = await fetch(verifyCaptchaResponseURL);
-  const data = await response.json({});
+  const data = await response.json();
 
   if (data.success === true) {
     const newMessage = new ContactModel(Message);
     await newMessage.save();
     res.redirect("/contact/success");
+  } else {
+    res.redirect("/contact");
   }
-  res.redirect("/contact");
 };
